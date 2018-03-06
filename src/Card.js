2ad23de0999ac3944287800979af06b9e332638a
Chris <info@interwebdesigner.co.uk>
2018-03-06 22:38:17 +0000

    added some styling

--- src/Card.js
+++ src/Card.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const Card = ({name, gender, id, height, mass, hair_color, skin_color, eye_color, birth_year}) => {
 	return (
-		<div className='tc bg-white dib br3 pa3 ma2 grow bw2 shadow-5'>
-			{/*<img alt='actors' src={`https://robohash.org/${id}?200x200`} />*/}
+		<div className='tc bg-white dib br3 pa3 ma2 grow bw2 shadow-5 card'>
 			<div>
 				<h2>{name}</h2>
 				<ul className="list">
